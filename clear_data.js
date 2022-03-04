49022d14f7b2545f5273c7e094d473d3f344a83e
Aleksandar Miladinović <squirrelosopher@gmail.com>
2022-03-04 22:32:39 +0100

    Added script for data clearing

---
+++ clear_data.js
@@ -0,0 +1,38 @@
+import 'dotenv/config';
+import Constants from './utils/constants.js';
+import redisRepository from './repository/redis_repository.js';
+
+import Debug from 'debug';
+const debug = Debug('redis-bigfoot-sightings:server');
+
+const clearData = async () => {
+    try {
+        const indexDestroyedResult = await redisRepository.destroyIndex();
+        debug(`index destroy status: ${indexDestroyedResult}`);
+
+        const keys = await redisRepository.getKeys(`${Constants.REDIS_SIGHTING_KEY}:*`);
+        if (keys.length > 0) {
+            debug(`previous data exists, total keys found: ${keys.length}`);
+            keys.forEach(key => redisRepository.pipeDeleteKey(key));
+        }
+
+        const result = await redisRepository.pipeExecute();
+        if (result.length > 0) {
+            let totalSuccessful = 0;
+            result.forEach(res => {
+                if (res[0] == null) {
+                    totalSuccessful++;
+                }
+            })
+
+            debug(`total Redis commands attempted: ${result.length}, successful: ${totalSuccessful}`);
+        }
+    } catch (error) {
+        console.error(`unable to clear data, reason: ${error.message}`);
+        debug(error.stack);
+    } finally {
+        redisRepository.disconnect();
+    }
+}
+
+clearData();
