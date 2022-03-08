e35b0027cfebd5338ef0e31fd4c406e8ec892b0d
squirrelosopher <squirrelosopher@gmail.com>
2022-03-08 14:42:08 +0100

    Fixed incorrect folder name

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 const __dirname = path.resolve();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'view'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
