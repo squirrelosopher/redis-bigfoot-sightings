b6ce7cdd314bae028d91efeef8ac7d16110b5525
squirrelosopher <squirrelosopher@gmail.com>
2022-03-11 09:31:03 +0100

    Add script to automatically open specific browser url (sightings page)

---
+++ open_url.js
@@ -0,0 +1,17 @@
+import open from 'open';
+import ConfigurationConstants from './src/util/configuration_constants.js';
+import ViewConstants from './src/util/view_constants.js';
+
+const url = `http://127.0.0.1:${ConfigurationConstants.SERVER_PORT}/${ViewConstants.SIGHTINGS}`;
+
+const openedUrlProcess = await open(url, {
+    wait: false
+});
+
+openedUrlProcess.on('spawn', () => {
+    console.log(`opened address ${url} [pid ${openedUrlProcess.pid}]`);
+});
+
+openedUrlProcess.on('error', (error) => {
+    console.log(`unable to open address ${url}, reason: ${error.message}`);
+});
