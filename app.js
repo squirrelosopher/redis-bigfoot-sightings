bdeec279b6b770b75b62f5109024e496d8e50d66
squirrelosopher <squirrelosopher@gmail.com>
2022-04-04 12:38:44 +0200

    Changed 'sightings' route to root route '/'

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', sightingsRouter);
 app.use(`/${ViewConstants.SIGHTING}`, sightingRouter);
-app.use(`/${ViewConstants.SIGHTINGS}`, sightingsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
